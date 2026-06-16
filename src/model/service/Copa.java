90f2541114086d72fed1603bfa4c272d24ac3340
FelipeS13 <felipe.silvalima@upe.br>
2026-06-16 02:38:36 -0300

    Adcionando gerenciamento d seleções

--- src/model/service/Copa.java
+++ src/model/service/Copa.java
@@ -6,6 +6,7 @@ import java.util.List;
 import model.Partida;
 import model.Selecao;
 
+
 public class Copa {
 
     private List<Selecao> selecoes;
@@ -16,5 +17,25 @@ public class Copa {
         selecoes = new ArrayList<>();
         partidas = new ArrayList<>();
         artilheiros = new ArrayList<>();
+    }
+
+    public void adicionarSelecao(Selecao selecao) {
+        selecoes.add(selecao);
+    }
+
+    public void registrarPartida(Partida partida) {
+        partidas.add(partida);
+    }
+
+    public List<Selecao> listarGrupo(char grupo) {
+        List<Selecao> grupoSelecoes = new ArrayList<>();
+
+        for (Selecao s : selecoes) {
+            if (s.getGrupo() == grupo) {
+                grupoSelecoes.add(s);
+            }
+        }
+
+        return grupoSelecoes;
     }
 }
